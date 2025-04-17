9b5c3c1e4636ae9b20c091329e8eeadd6c229e57
gjack1978 <gacouto@gmail.com>
2025-04-16 22:37:13 -0300

    feat: Use environment variables for Supabase config

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Substitua pelos valores do seu projeto Supabase
-const supabaseUrl = 'https://kysauxdsdzhghptfcpjy.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt5c2F1eGRzZHpoZ2hwdGZjcGp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4NDU4NzksImV4cCI6MjA2MDQyMTg3OX0.xXSUNF5hDY7bpVe92MqmCjp-loo8mkuBnu4yuG-AS3k';
+// Use environment variables
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// Check if variables are set
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL and Anon Key must be defined in environment variables.");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
